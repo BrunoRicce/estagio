9e81c0996bef272927fe3c7390d7997fd7436640
Bruno Ricce <brunoricce01@gmail.com>
2021-12-03 23:00:21 -0300

    bd correto

--- database/connection.js
+++ database/connection.js
@@ -2,8 +2,8 @@ const mysql = require("mysql2/promise");
 
 const connection = mysql.createPool({
     host: "localhost",
-    user: "root",
-    password: "",
+    user: "remote2",
+    password: "*fdgr132COYjhq",
     database: "slw",
     //multipleStatements: true
 });
